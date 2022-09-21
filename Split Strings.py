3e9ad5fa401c7c87f6d744e7f3931d19f966824c
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 15:24:05 -0400

    Update Split Strings.py

--- Split Strings.py
+++ Split Strings.py
@@ -1,3 +1,24 @@
+"""                                                                      Problem:
+
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing
+second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+"""
+
+"""                                                                      Solution:
+
+If the length of s is divisible by 2, append the characters of s as elements of w in pairs of 2. If the length of s is odd then append all of the characters except
+for the last one as elements of w in pairs of two and then append the last character with an added '_'.
+
+Return w
+
+"""
+
 def solution(s):
     w = []
     if len(s) % 2 == 0:
