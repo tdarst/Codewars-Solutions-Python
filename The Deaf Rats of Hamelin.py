9ed7d5990da973d2be26739791cf4264ba35b2f6
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 13:57:48 -0400

    Update The Deaf Rats of Hamelin.py

--- The Deaf Rats of Hamelin.py
+++ The Deaf Rats of Hamelin.py
@@ -1,3 +1,33 @@
+"""                                                                      Problem:
+
+Story
+The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+
+But some of the rats are deaf and are going the wrong way!
+
+Kata Task
+How many deaf rats are there?
+
+Legend
+P = The Pied Piper
+O~ = Rat going left
+~O = Rat going right
+Example
+ex1 ~O~O~O~O P has 0 deaf rats
+
+ex2 P O~ O~ ~O O~ has 1 deaf rat
+
+ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
+
+"""
+
+"""                                                                      Solution:
+
+Split the string into two by the location of the piper and then delete the whitespace in each seperated string. Loop through and search for all rats facing each other
+and replace them with a rat going the correct direction and then loop through again to clean up the remaining tails. Check if the piper is at the beginning or the end
+of the string before returning the total length divided by two giving the amount of rats that weren't facing the correct direction (the "deaf rats")
+
+"""
 def count_deaf_rats(t):
     print(t)
     t = t.split('P')
