5b2968e9b360b60e94f264655bd5605b73c16778
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 15:48:02 -0400

    Update CamelCase Method.py

--- CamelCase Method.py
+++ CamelCase Method.py
@@ -1,2 +1,20 @@
+"""                                                                      Problem:
+
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter
+capitalized without spaces.
+
+For instance:
+
+camelcase("hello case") => HelloCase
+camelcase("camel case word") => CamelCaseWord
+
+"""
+
+"""                                                                      Solution:
+
+Since the words are already spaced we can use the title() method to capitalize them all and then remove the spaces. Was proud of doing this one in one line.
+
+"""
+
 def camel_case(s):
     return s.title().replace(" ", "")
