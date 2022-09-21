3d06dd120e7ab375c66a5bf678ecd3b3a0f3bbeb
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 13:41:14 -0400

    Update Equal Sides Of An Array.py

--- Equal Sides Of An Array.py
+++ Equal Sides Of An Array.py
@@ -1,3 +1,13 @@
+"""                                                                      Problem:
+
+
+"""
+
+"""                                                                      Solution:                                                                              """
+
+""" create one integer value that's the full sum of the array and one equal to 0 and then add to the 0 value and subtract fro mthe full sum until they're even with the
+subtracting and adding values being the numbers in the array that the for loop is indexing through. If an equivalence cannot be reached return False """
+
 def find_even_index(arr):
     rSide = sum(arr)
     lSide = 0
