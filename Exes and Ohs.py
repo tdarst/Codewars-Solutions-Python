68dff3779679c26b2cc780d84e5aecde4542122a
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 16:06:52 -0400

    Update Exes and Ohs.py

--- Exes and Ohs.py
+++ Exes and Ohs.py
@@ -1,3 +1,23 @@
+"""                                                                      Problem:
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+"""
+
+"""                                                                      Solution:
+
+Make the passed-in string lowercase and then if the amount of x's minus the amount of o's is the same return True. Else return False.
+
+"""
+
 def xo(s):
     s = s.lower()
     if s.count('x') - s.count('o') == 0:
