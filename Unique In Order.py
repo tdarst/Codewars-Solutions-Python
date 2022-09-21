dcfa2f79a6fe2c756e71d6ec82186f306c84ce18
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-21 15:13:14 -0400

    Update Unique In Order.py

--- Unique In Order.py
+++ Unique In Order.py
@@ -1,3 +1,25 @@
+"""                                                                      Problem:
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and 
+preserving the original order of elements.
+
+For example:
+
+uniqueInOrder("AAAABBBCCDAABBB") == {'A', 'B', 'C', 'D', 'A', 'B'}
+uniqueInOrder("ABBCcAD")         == {'A', 'B', 'C', 'c', 'A', 'D'}
+uniqueInOrder([1,2,2,3,3])       == {1,2,3}
+
+"""
+
+"""                                                                      Solution:
+
+Create an empty list and fill it with the elements of the passed-in list if that element has not already been appended or is not a repeating element. This allows for 
+the order of the elements to be preserved without repeating elements to be added.
+
+Return the new list "l".
+
+"""
+
 def unique_in_order(i):
     l = []
     for x in range(0, len(i)):
