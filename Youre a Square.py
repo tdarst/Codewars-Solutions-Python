e87184e6fabff80f7a761ffe7deb156a76c2ad23
tdarst <51347370+tdarst@users.noreply.github.com>
2022-10-13 15:19:22 -0400

    Update Youre a Square.py

--- Youre a Square.py
+++ Youre a Square.py
@@ -1,3 +1,15 @@
+"""                                                                      Problem:
+
+Given an integral number, determine if it's a square number:
+
+"""
+
+"""                                                                      Solution:
+
+Yay for short-circuit evaluation! Simply check if n is less than 0 or if (n's rounded-down square)^2 == n to determine if it is a perfect square or not.
+
+"""
+
 import math
 def is_square(n):    
     return False if (n < 0) or (int(math.sqrt(n))**2 != n) else True
