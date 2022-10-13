97f083ba940661c22f1df1c769f8c3c349f6f2b2
tdarst <51347370+tdarst@users.noreply.github.com>
2022-10-13 15:21:35 -0400

    Update Youre a Square.py

--- Youre a Square.py
+++ Youre a Square.py
@@ -13,3 +13,11 @@ Yay for short-circuit evaluation! Simply check if n is less than 0 or if (n's ro
 import math
 def is_square(n):    
     return False if (n < 0) or (int(math.sqrt(n))**2 != n) else True
+
+"""                                                                     What I learned:
+
+This is probably best practice
+
+return n > -1 and math.sqrt(n) % 1 == 0;
+
+"""
